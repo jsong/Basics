06150c24e6e18658b1b3470ed178d2957f24f305
Jian Song <simjian@gmail.com>
2018-04-23 22:46:24 -0700

    270 find the closet node value of target on BST

--- eclipse-workspace/BinaryTree/src/com/general/binarytree/BSTIterator.java
+++ eclipse-workspace/BinaryTree/src/com/general/binarytree/BSTIterator.java
@@ -5,16 +5,34 @@ import java.util.Stack;
 public class BSTIterator {
 	private Stack<TreeNode> stack;
 	private TreeNode cur;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
+	// 270. Closest Binary Search Tree Value
+	// Company: Microsoft Snapchat Google
+	// Description: Find the value in BST which is closet to the target. Non empty BST.
+	// Solution: Iteration to find out which nodes is closet to target value. And return that node value.
+	public int closestValue(TreeNode root, double target) {
+		int res = root.val;
+		while (root != null) {
+			if (Math.abs(target - root.val) < Math.abs(target - res)) {
+				res = root.val;
+			}
+			
+			root = root.val > target ? root.left: root.right;
+		}
+		
+		return res;
+	}
+	
 	// 173. Binary Tree Iterator
 	// Company: Facebook LinkedIn
 	// Description: Use in-order for BST tree, which will output the sorted numbers.
 	// Solution: Inorder traversal to find the next smallest value.
-	
+
 	public BSTIterator(TreeNode root) {
 		cur = root;
 		stack = new Stack<>();
@@ -22,7 +40,8 @@ public class BSTIterator {
 
 	/** @return whether we have a next smallest number */
 	public boolean hasNext() {
-		if (!stack.isEmpty() || cur != null) return true;
+		if (!stack.isEmpty() || cur != null)
+			return true;
 		return false;
 	}
 
