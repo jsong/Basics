3e3bdae6eb634861b9f203a5da4ce1b32ddb15b4
osx <osx@osxs-MacBook-Pro.local>
2018-07-22 12:05:08 -0700

    15 3Sum Solution with left and right pointers

--- eclipse-workspace/Array/src/com/leetcode/array/Solution.java
+++ eclipse-workspace/Array/src/com/leetcode/array/Solution.java
@@ -3,6 +3,8 @@ package com.leetcode.array;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Solution {
 
@@ -18,27 +20,59 @@ public class Solution {
 	// 1. Two Sum
 	// Company: Facebook Microsoft Amazon Bloomberg LinkedIn Apple Airbnb Yelp Yahoo
 	// Adobe Dropbox
-	// Description: Given an array, return indices of the two numbers such that they can 
+	// Description: Given an array, return indices of the two numbers such that they
+	// can
 	// add up to a specific target.
-	// Solution: Use Hashmap to store the key and indexes, if target - cur has a match, then return.
+	// Solution: Use Hashmap to store the key and indexes, if target - cur has a
+	// match, then return.
 	public int[] twoSum(int[] nums, int target) {
 		HashMap<Integer, Integer> map = new HashMap<>();
-		
+
 		for (int i = 0; i < nums.length; i++) {
 			if (map.containsKey(target - nums[i])) {
 				int j = map.get(target - nums[i]);
-				return new int[] {i, j};
+				return new int[] { i, j };
 			}
 			map.put(nums[i], i);
 		}
-	
+
 		return new int[] {};
 	}
 
-	//
-	// Company:
-	// Description:
-	// Solution:
+	// 15. 3Sum
+	// Company: Facebook Microsoft Amazon Bloomberg Adobe Works Applications
+	// Description: Given an array nums of n integers, are there elements a, b, c in
+	// nums such that a + b + c = 0?
+	// Solution: First sort, and then anchor the i, use two pointers left and right 
+	// to find the other two elements.
+	public List<List<Integer>> threeSum(int[] nums) {
+		Arrays.sort(nums);
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		
+		for (int i = 0; i < nums.length - 2; i++) { // use the right except i to find the other sum 
+			int left = i + 1; 
+			int right = nums.length - 1;
+			int sum = 0 - nums[i];
+			
+			if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+				while (left < right) {
+					if (nums[left] + nums[right] == sum) {
+						res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+						while (left < right && nums[left] == nums[left + 1]) left++; //remove left duplicates
+						while (left < right && nums[right] == nums[right -1]) right--; // remove right duplicates
+						left++;
+						right--;
+					} else if (nums[left] + nums[right] < sum) {
+						left++;
+					} else {
+						right--;
+					}
+				}
+			}
+		}
+		
+		return res;
+	}
 
 	//
 	// Company:
