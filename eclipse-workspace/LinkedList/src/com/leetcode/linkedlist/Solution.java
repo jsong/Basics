de45754a68da64e4b06e975a4f4a69c7bc0b188a
Jian Song <simjian@gmail.com>
2018-08-02 19:02:08 -0700

    328. Odd Even LinkedList. TODO: Check better solution

--- eclipse-workspace/LinkedList/src/com/leetcode/linkedlist/Solution.java
+++ eclipse-workspace/LinkedList/src/com/leetcode/linkedlist/Solution.java
@@ -4,7 +4,15 @@ public class Solution {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//// 1-> 2 -> 3 -> 4 -> 5;
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = new ListNode(4);
+		head.next.next.next.next = new ListNode(5);
+		
+		Solution sl = new Solution();
+		sl.oddEvenList(head);
 	}
 
 	// 206. Reverse Linked List
@@ -14,7 +22,7 @@ public class Solution {
 	// Description: Reverse a singly linked list.
 	// eg. Input: 1->2->3->4->5->NULL
 	// Output: 5->4->3->2->1->NULL
-	// Solution: Use Two pointers iteratively until one of them reaches the end. 
+	// Solution: Use Two pointers iteratively until one of them reaches the end.
 	public ListNode reverseList(ListNode head) {
 		ListNode curNode = head;
 		ListNode preNode = null;
@@ -40,12 +48,68 @@ public class Solution {
 	// Output: 1->3->5->2->4->NULL
 	// Solution:
 	public ListNode oddEvenList(ListNode head) {
-
+		if (head == null || head.next == null) {
+			
+		}
+		
+		ListNode evenHeadNode = null;
+		ListNode oddHeadNode = null;
+		ListNode curNode = head;
+		ListNode newEvenHeadNode = null;
+		ListNode newOddHeadNode = null;
+		int counter = 0;
+		
+		while (curNode != null) {
+			++counter; 
+			if (counter % 2 == 1) {
+				if (oddHeadNode == null) {
+					oddHeadNode = curNode;
+					newOddHeadNode = oddHeadNode;
+				} else {
+					oddHeadNode.next = curNode;
+					oddHeadNode = oddHeadNode.next;
+				}
+			} else {
+				if (evenHeadNode == null) {
+					evenHeadNode = curNode;
+//					newEvenHeadNode = curNode; // what if newEvenHeadNode point to evenHeadNode, does it change accrodingly? 
+					newEvenHeadNode = evenHeadNode;
+				} else {
+					evenHeadNode.next = curNode;
+					evenHeadNode = evenHeadNode.next;
+				}
+			}
+			curNode = curNode.next;
+		}
+		
+		oddHeadNode.next = newEvenHeadNode;
+		evenHeadNode.next = null;
+		
+		return newOddHeadNode;
 	}
 
-	// 206. Reverse Linked List
+	//
 	// Company:
 	// Description:
 	// Solution:
 
+	//
+	// Company:
+	// Description:
+	// Solution:
+
+	//
+	// Company:
+	// Description:
+	// Solution:
+
+	//
+	// Company:
+	// Description:
+	// Solution:
+
+	//
+	// Company:
+	// Description:
+	// Solution:
 }
