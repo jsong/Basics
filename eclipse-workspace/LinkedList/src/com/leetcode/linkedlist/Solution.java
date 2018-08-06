fafcc8119d94dc259807c8c6be40291e30fc9829
osx <osx@osxs-MacBook-Pro.local>
2018-08-05 22:09:01 -0700

    83. Remove Duplicates from Sorted List

--- eclipse-workspace/LinkedList/src/com/leetcode/linkedlist/Solution.java
+++ eclipse-workspace/LinkedList/src/com/leetcode/linkedlist/Solution.java
@@ -16,16 +16,17 @@ public class Solution {
 		// TODO Auto-generated method stub
 		//// 1-> 2 -> 3 -> 4 -> 5;
 		ListNode head = new ListNode(1);
-		head.next = new ListNode(2);
-		head.next.next = new ListNode(3);
-		head.next.next.next = new ListNode(4);
-		head.next.next.next.next = new ListNode(5);
+		head.next = new ListNode(1);
+		head.next.next = new ListNode(2);
+		head.next.next.next = new ListNode(3);
+		head.next.next.next.next = new ListNode(3);
 		Solution sl = new Solution();
 		// sl.oddEvenList(head);
 		///
 		/// 243, 564
-//		sl.reverseBetween(head, 2, 4);
-		sl.partition(head, 3);
+		// sl.reverseBetween(head, 2, 4);
+//		sl.partition(head, 3);
+		sl.deleteDuplicates(head);
 	}
 
 	// 206. Reverse Linked List
@@ -153,60 +154,79 @@ public class Solution {
 		ListNode dummy = new ListNode(-1);
 		dummy.next = head;
 		ListNode pre = dummy; // after which is the last node
-		
+
 		for (int i = 0; i < m - 1; i++) {
 			pre = pre.next;
 		}
-		
-		ListNode head2 = pre; 
+
+		ListNode head2 = pre;
 		pre = pre.next;
 		ListNode cur = pre.next;
-		
+
 		for (int i = m; i < n; i++) {
 			pre.next = cur.next;
 			cur.next = head2.next;
-			head2.next = cur; //1 -> 3, 1 -> 4...
+			head2.next = cur; // 1 -> 3, 1 -> 4...
 			cur = pre.next;
 		}
-		
+
 		return dummy.next;
 	}
 
 	// 86. Partition List
 	// Company: Microsoft Yahoo Adobe
-	// Description: Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x. You should preserve the original relative order of the nodes in each of the two partitions.
-	// Solution: Use two linkedlist, left and right, left to record the number smaller, right to record the number larger and combine the linkedlist.
+	// Description: Given a linked list and a value x, partition it such that all
+	// nodes less than x come before nodes greater than or equal to x. You should
+	// preserve the original relative order of the nodes in each of the two
+	// partitions.
+	// Solution: Use two linkedlist, left and right, left to record the number
+	// smaller, right to record the number larger and combine the linkedlist.
 	public ListNode partition(ListNode head, int x) {
-        ListNode left_dummy = new ListNode(-1);
-        ListNode right_dummy = new ListNode(-1);
-        ListNode left_cur = left_dummy;
-        ListNode right_cur = right_dummy;
-        
+		ListNode left_dummy = new ListNode(-1);
+		ListNode right_dummy = new ListNode(-1);
+		ListNode left_cur = left_dummy;
+		ListNode right_cur = right_dummy;
+
+		ListNode cur = head;
+
+		while (cur != null) {
+			if (cur.val < x) {
+				left_cur.next = cur;
+				left_cur = left_cur.next; // iterate.
+			} else {
+				right_cur.next = cur;
+				right_cur = cur;
+			}
+			cur = cur.next;
+		}
+
+		left_cur.next = right_dummy.next;
+		right_cur.next = null;
+
+		return left_dummy.next;
+	}
+
+	// 83. Remove Duplicates from Sorted List
+	// Company: Amazon Alibaba
+	// Description: Given a sorted linked list, delete all duplicates such that each element appear only once.
+	// eg. 1 -> 1 -> 1 -> 2 ==> 1 -> 2
+	// Solution: iterate the linked list, while cur.next != null && cur.val == cur.next.val, iterate to next.
+	public ListNode deleteDuplicates(ListNode head) {
         ListNode cur = head;
         
-        while (cur != null) {
-        	if (cur.val < x) {
-        		left_cur.next = cur;
-        		left_cur = left_cur.next;  //iterate.
-        	} else {
-        		right_cur.next = cur;
-        		right_cur = cur;
+        while (cur != null && cur.next != null) {
+        	while (cur.next != null && cur.val == cur.next.val) {
+        		cur.next = cur.next.next;
         	}
+        	
         	cur = cur.next;
         }
         
-        left_cur.next = right_dummy.next;
-        right_cur.next = null;
-        
-        return left_dummy.next;
+        return head;
     }
-	//
-	// Company:
-	// Description:
-	// Solution:
 
-	//
-	// Company:
-	// Description:
+	// 82. Remove Duplicates from Sorted List II
+	// Company: Microsoft Amazon Baidu Bloomberg
+	// Description: Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
 	// Solution:
 }
