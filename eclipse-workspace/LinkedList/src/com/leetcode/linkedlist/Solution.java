294b1ff6288bfea91ed4c6cbe2d01192d567906c
osx <osx@osxs-MacBook-Pro.local>
2018-08-05 21:48:30 -0700

    86. Partition List

--- eclipse-workspace/LinkedList/src/com/leetcode/linkedlist/Solution.java
+++ eclipse-workspace/LinkedList/src/com/leetcode/linkedlist/Solution.java
@@ -24,7 +24,8 @@ public class Solution {
 		// sl.oddEvenList(head);
 		///
 		/// 243, 564
-		sl.reverseBetween(head, 2, 4);
+//		sl.reverseBetween(head, 2, 4);
+		sl.partition(head, 3);
 	}
 
 	// 206. Reverse Linked List
@@ -171,11 +172,34 @@ public class Solution {
 		return dummy.next;
 	}
 
-	//
-	// Company:
-	// Description:
-	// Solution:
-
+	// 86. Partition List
+	// Company: Microsoft Yahoo Adobe
+	// Description: Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x. You should preserve the original relative order of the nodes in each of the two partitions.
+	// Solution: Use two linkedlist, left and right, left to record the number smaller, right to record the number larger and combine the linkedlist.
+	public ListNode partition(ListNode head, int x) {
+        ListNode left_dummy = new ListNode(-1);
+        ListNode right_dummy = new ListNode(-1);
+        ListNode left_cur = left_dummy;
+        ListNode right_cur = right_dummy;
+        
+        ListNode cur = head;
+        
+        while (cur != null) {
+        	if (cur.val < x) {
+        		left_cur.next = cur;
+        		left_cur = left_cur.next;  //iterate.
+        	} else {
+        		right_cur.next = cur;
+        		right_cur = cur;
+        	}
+        	cur = cur.next;
+        }
+        
+        left_cur.next = right_dummy.next;
+        right_cur.next = null;
+        
+        return left_dummy.next;
+    }
 	//
 	// Company:
 	// Description:
