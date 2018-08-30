4ae74e037f80325cb86ca79261e6f0aba6b2d651
osx <osx@osxs-MacBook-Pro.local>
2018-08-29 23:44:13 -0700

    10. Regular Expression Matching

--- eclipse-workspace/String/src/com/leetcode/string/Solution.java
+++ eclipse-workspace/String/src/com/leetcode/string/Solution.java
@@ -599,8 +599,25 @@ public class Solution {
 	// element.
 	// Solution: 1. DP solution, 2. Use dfs + visited memo.
 
-	public boolean isMatch(String s, String p) {
-
+	public boolean isMatchRegularExpression(String s, String p) {
+		int m = s.length();
+		int n = p.length();
+		
+		boolean[][] dp = new boolean[m + 1][n + 1];
+		dp[0][0] = true;
+		
+		for (int i = 0; i <=m; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (p.charAt(j - 1) == '*') {
+					dp[i][j] = dp[i][j - 2] ||  // * act as empty
+							   (i > 0) && dp[i - 1][j] && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.'); // repeat once. 
+				} else {
+					dp[i][j] = (i > 0) && dp[i - 1][j - 1] && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.');  
+				}
+			}
+		}
+		
+		return dp[m][n];
 	}
 
 	// 44. Wildcard Matching
