1856a096183cbffb017be8480c010b8cc9666178
osx <osx@osxs-MacBook-Pro.local>
2018-09-19 23:18:22 -0700

    21. Merge Two Sorted Lists

--- eclipse-workspace/Algorithm_Sort/src/com/leetcode/sort/Solution.java
+++ eclipse-workspace/Algorithm_Sort/src/com/leetcode/sort/Solution.java
@@ -11,67 +11,106 @@ class ListNode {
 }
 
 public class Solution {
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ListNode head = new ListNode(3);
 		head.next = new ListNode(1);
 		head.next.next = new ListNode(5);
-		
+
 		Solution sl = new Solution();
 		ListNode sorted = sl.insertionSortList(head);
 		System.out.println("Insertion Sort Complexity O(n ^ 2)");
 	}
-	
+
 	// ## Insertion Sort
 	// 147. Insertion Sort List
 	// Company: Apple # Adobe Alibaba Google Airbnb
 	// Description: Sort a linked list using insertion sort.
-	// Solution: Start a new dummy linked list, start from beginning, scan current linked list, check whehter cur.val 
-	// is bigger than dummy linked list, if not return the previous node in the dummy linked list, and append the current 
+	// Solution: Start a new dummy linked list, start from beginning, scan current
+	// linked list, check whehter cur.val
+	// is bigger than dummy linked list, if not return the previous node in the
+	// dummy linked list, and append the current
 	// behind.
 	public ListNode insertionSortList(ListNode head) {
-        ListNode dummy = new ListNode(Integer.MIN_VALUE);
-        
-        for (ListNode cur = head; cur != null;) {
-        	ListNode pos = findInsertPos(dummy, cur.val);
-        	ListNode tmp = cur.next;
-        	cur.next = pos.next;
-        	pos.next = cur;
-        	cur = tmp;
-        }
-        
-        return dummy.next;
-    }
-	
+		ListNode dummy = new ListNode(Integer.MIN_VALUE);
+
+		for (ListNode cur = head; cur != null;) {
+			ListNode pos = findInsertPos(dummy, cur.val);
+			ListNode tmp = cur.next;
+			cur.next = pos.next;
+			pos.next = cur;
+			cur = tmp;
+		}
+
+		return dummy.next;
+	}
+
 	private ListNode findInsertPos(ListNode node, int val) {
 		ListNode pre = null;
-		
+
 		for (ListNode cur = node; cur != null && cur.val <= val; pre = cur, cur = cur.next) {
-			
+
 		}
-		
+
 		return pre;
 	}
 
-	
 	// ## Merge Sort
 	// 88. Merge Sorted Array
-	// Company: Facebook Microsoft Amazon Indeed Baidu # Adobe Bloomberg Yelp Google Expedia Intel Quip
-	// Description: Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
-	// Solution: From back to front while both the arrays indicator still > 0, choose bigger from two arrays.
+	// Company: Facebook Microsoft Amazon Indeed Baidu # Adobe Bloomberg Yelp Google
+	// Expedia Intel Quip
+	// Description: Given two sorted integer arrays nums1 and nums2, merge nums2
+	// into nums1 as one sorted array.
+	// Solution: From back to front while both the arrays indicator still > 0,
+	// choose bigger from two arrays.
 	// if ib still > 0, then we need to put it back.
-	 public void merge(int[] nums1, int m, int[] nums2, int n) {
-	       int ia = m - 1;
-	       int ib = n - 1;
-	       int ic = m + n - 1;
-	       
-	       while (ia >= 0 && ib >= 0) {
-	    	   nums1[ic--] = nums1[ia] >= nums2[ib] ? nums1[ia--]: nums2[ib--];
-	       }
-	       
-	       while (ib >= 0) {
-	    	   nums1[ic--] = nums2[ib--];
-	       }
-	 }
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+		int ia = m - 1;
+		int ib = n - 1;
+		int ic = m + n - 1;
+
+		while (ia >= 0 && ib >= 0) {
+			nums1[ic--] = nums1[ia] >= nums2[ib] ? nums1[ia--] : nums2[ib--];
+		}
+
+		while (ib >= 0) {
+			nums1[ic--] = nums2[ib--];
+		}
+	}
+
+	// 21. Merge Two Sorted Lists
+	// Company: Microsoft Amazon Facebook Adobe Alibaba Tencent Warmart labs Apple Indeed Google # Uber Yahoo Baidu Yelp Bloomberg Arista Networks LinkedIn Hulu Intuit Groupon
+	// Description: Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+	// Solution: Iterate both pointers and set cur pointers. 
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		ListNode dummy = new ListNode(-1);
+		ListNode cur = dummy;
+		
+		while (l1 != null && l2 != null) {
+			if (l1.val <= l2.val) {
+				cur.next = l1;
+				l1 = l1.next;
+			} else {
+				cur.next = l2;
+				l2 = l2.next;
+			}
+			
+			cur = cur.next;
+		}
+		
+		while (l1 != null) {
+			cur.next = l1;
+			cur = cur.next;
+			l1 = l1.next;
+		}
+		
+		while (l2 != null) {
+			cur.next = l2;
+			cur = cur.next;
+			l2 = l2.next;
+		}
+		
+		return dummy.next;
+	}
 }
