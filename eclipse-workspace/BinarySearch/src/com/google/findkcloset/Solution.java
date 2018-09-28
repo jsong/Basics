e827a6a3972039cf0eb4c443a9b7cd2f9488ef25
osx <osx@osxs-MacBook-Pro.local>
2018-09-27 23:22:07 -0700

    33. Search in Rotated Sorted Array

--- eclipse-workspace/BinarySearch/src/com/google/findkcloset/Solution.java
+++ eclipse-workspace/BinarySearch/src/com/google/findkcloset/Solution.java
@@ -1,4 +1,5 @@
 package com.google.findkcloset;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -8,55 +9,57 @@ import java.util.Map;
 public class Solution {
 
 	public static void main(String[] args) {
-		int[] arr = new int[] {0,0,0,1,3,5,6,7,8,8};
-		//[0,0,0,1,3,5,6,7,8,8]
-//		2
-//		2
+		int[] arr = new int[] { 0, 0, 0, 1, 3, 5, 6, 7, 8, 8 };
+		// [0,0,0,1,3,5,6,7,8,8]
+		// 2
+		// 2
 		Solution sl = new Solution();
 		List<Integer> re = sl.findClosestElements(arr, 2, 2);
 		System.out.println(re.size());
-		int[] array = new int[] {1,3,5};
+		int[] array = new int[] { 1, 3, 5 };
 		int index = sl.search(array, 0);
-		 
+
 	}
-	
-	
-	//153. Find Minimum in Rotated Sorted Array
-	//Microsoft
-	//Find first element <= target
+
+	// 153. Find Minimum in Rotated Sorted Array
+	// Microsoft
+	// Find first element <= target
 	public int findMin(int[] nums) {
-		if (nums ==null || nums.length == 0) {
+		if (nums == null || nums.length == 0) {
 			return -1;
 		}
-		
-		int start = 0; 
+
+		int start = 0;
 		int end = nums.length - 1;
 		int target = nums[end];
 		while (start + 1 < end) {
-			int mid = start + (end - start)/2; 
+			int mid = start + (end - start) / 2;
 			if (nums[mid] <= target) {
 				end = mid;
 			} else {
 				start = mid;
 			}
 		}
-		if(nums[start] <= target) {
+		if (nums[start] <= target) {
 			return nums[start];
 		} else {
 			return nums[end];
 		}
-		
-    }
-	//162. Find Peak Element Microsoft & Google. Please note the following solution could also be used
-	//to this question. But note, index vs value.
-	//585. Maximum Number in Mountain Sequence LintCode
-	//Given a mountain sequence of n integers which increase first and then decrease, find the top.
+
+	}
+
+	// 162. Find Peak Element Microsoft & Google. Please note the following solution
+	// could also be used
+	// to this question. But note, index vs value.
+	// 585. Maximum Number in Mountain Sequence LintCode
+	// Given a mountain sequence of n integers which increase first and then
+	// decrease, find the top.
 	public int mountainSequence(int[] nums) {
 		int start = 0, end = nums.length - 1;
-//		int end = nums.length - 1;
+		// int end = nums.length - 1;
 		while (start + 1 < end) {
-			int m1 = start + (end - start)/2;
-			int m2 = end - (end - m1)/2;
+			int m1 = start + (end - start) / 2;
+			int m2 = end - (end - m1) / 2;
 			if (nums[m1] < nums[m2]) {
 				start = m1 + 1;
 			} else if (nums[m1] > nums[m2]) {
@@ -66,111 +69,109 @@ public class Solution {
 				end = m2;
 			}
 		}
-		
-		return nums[start] > nums[end] ? nums[start]: nums[end];
+
+		return nums[start] > nums[end] ? nums[start] : nums[end];
 	}
-	
-	
-	
-	//50. Pow(x,n)
-	//n could be negative. Beware not use for ... n loop.
-	//cause it will cause overflow. use divide and concur. 
-	//Company Google Facebook.
-	 public double myPow(double x, int n) {
-	        long N = n;
-	        if (N < 0) {
-	            x = 1 / x;
-	            N = -N;
-	        }
-	        double ans = 1, tmp = x;
-	        while (N != 0) {
-	            if (N % 2 == 1) {
-	                ans = ans * tmp;
-	            }
-	            tmp *= tmp;
-	            N = N/2;
-	        }
-	        /* O(n) will exceed time limit.
-	        for (long i = 0; i < N; i++)
-	            ans = ans * x;
-	        */
-	        return ans;
-	    }
-	
-	//33. Search in Rotated Sorted Array
-	//Search in a rotated array.
-	 public int search(int[] nums, int target) {
-		 if (nums ==null || nums.length == 0) {
-				return -1;
+
+	// 50. Pow(x,n)
+	// n could be negative. Beware not use for ... n loop.
+	// cause it will cause overflow. use divide and concur.
+	// Company Google Facebook.
+	public double myPow(double x, int n) {
+		long N = n;
+		if (N < 0) {
+			x = 1 / x;
+			N = -N;
+		}
+		double ans = 1, tmp = x;
+		while (N != 0) {
+			if (N % 2 == 1) {
+				ans = ans * tmp;
 			}
-			
-			int start = 0; 
-			int end = nums.length - 1;
-			while (start + 1 < end) {
-				int mid = start + (end - start)/2; 
-				if (target == nums[mid]) {
-					return mid;
-				} else if (nums[start] <= nums[mid]) {
-					if (target < nums[mid] && target >= nums[start]) {
-                     end = mid;
-                 } else {
-                     start = mid;
-                 }
-				} else if (nums[mid] <= nums[end]) {
-                 if (target <= nums[end] && target > nums[mid]) {
-                     start = mid;
-                 } else {
-                     end = mid;
-                 }
+			tmp *= tmp;
+			N = N / 2;
+		}
+		/*
+		 * O(n) will exceed time limit. for (long i = 0; i < N; i++) ans = ans * x;
+		 */
+		return ans;
+	}
+
+	// 33. Search in Rotated Sorted Array
+	// Search in a rotated array.
+	public int search(int[] nums, int target) {
+		if (nums == null || nums.length == 0) {
+			return -1;
+		}
+
+		int start = 0;
+		int end = nums.length - 1;
+		
+		while (start + 1 < end) {
+			int mid = start + (end - start) / 2;
+			if (target == nums[mid]) {
+				return mid;
+			} else if (nums[start] <= nums[mid]) {
+				if (target < nums[mid] && target >= nums[start]) {
+					end = mid;
+				} else {
+					start = mid;
+				}
+			} else if (nums[mid] <= nums[end]) {
+				if (target <= nums[end] && target > nums[mid]) {
+					start = mid;
+				} else {
+					end = mid;
 				}
 			}
-			
-			if (nums[start] == target) {
-				return start;
-			} else if (nums[end] == target) {
-				return end;
-			}
-			
-			return -1;
-	 }
-	
-	//658. Find K Closest Elements
-	//extend the space to mid + k and starts from there.
+		}
+
+		if (nums[start] == target) {
+			return start;
+		} else if (nums[end] == target) {
+			return end;
+		}
+
+		return -1;
+	}
+
+	// 658. Find K Closest Elements
+	// extend the space to mid + k and starts from there.
 	//
-	  public List<Integer> findClosestElements2(int[] arr, int k, int x) {
-          int start = 0;
-          int end = arr.length - k;
-          List<Integer> res = new ArrayList<Integer>();
-          while (start < end) {
-              int mid = start + (end - start)/2;
-              if (x > arr[mid]) {
-                  if (x - arr[mid] > arr[mid + k] - x) {
-                      start = mid + 1;
-                  } else {
-                      end = mid;
-                  }
-              } else {
-                  end = mid;
-              }
-          }
-          
-          int index = start; 
-          while (k != 0) {
-              res.add(arr[index++]);
-              k--;
-          }
-          return res;
-      }
-	
-	//k is count, x is target.
+	public List<Integer> findClosestElements2(int[] arr, int k, int x) {
+		int start = 0;
+		int end = arr.length - k;
+		List<Integer> res = new ArrayList<Integer>();
+		while (start < end) {
+			int mid = start + (end - start) / 2;
+			if (x > arr[mid]) {
+				if (x - arr[mid] > arr[mid + k] - x) {
+					start = mid + 1;
+				} else {
+					end = mid;
+				}
+			} else {
+				end = mid;
+			}
+		}
+
+		int index = start;
+		while (k != 0) {
+			res.add(arr[index++]);
+			k--;
+		}
+		return res;
+	}
+
+	// k is count, x is target.
 	public List<Integer> findClosestElements(int[] arr, int k, int x) {
 		List<Integer> list = new ArrayList<Integer>();
-		//empty check? 
+		// empty check?
 		int left = findLowerCloset(arr, x);
 		int right = left + 1;
 		int count = 0;
 		int[] res = new int[k];
-		
+
 		while (count < k - 1) {
 			if (isLeftCloser(arr, x, left, right)) {
 				res[count++] = arr[left];
@@ -180,10 +181,10 @@ public class Solution {
 				right++;
 			}
 		}
-		
+
 		Arrays.sort(res);
-		
-		for (int i: res) {
+
+		for (int i : res) {
 			list.add(i);
 		}
 		return list;
@@ -199,16 +200,17 @@ public class Solution {
 		if (target - arr[left] < arr[right] - target) {
 			return true;
 		}
-		
+
 		return true;
 	}
-	
+
 	private int findLowerCloset(int[] arr, int target) {
-		int start = 0; int end = arr.length - 1;
-		//after the loop, start and end should be close to each other, then 
-		//start from there.
+		int start = 0;
+		int end = arr.length - 1;
+		// after the loop, start and end should be close to each other, then
+		// start from there.
 		while (start + 1 < end) {
-			int mid = start + (end - start)/2;
+			int mid = start + (end - start) / 2;
 			if (arr[mid] == target) {
 				end = mid;
 			} else if (arr[mid] > target) {
@@ -218,16 +220,16 @@ public class Solution {
 			}
 		}
 		System.out.println("start:" + start + " end:" + end + " target:" + target);
-		
-		//find last position of target.
+
+		// find last position of target.
 		if (arr[end] < target) {
 			return end;
 		}
 		if (arr[start] < target) {
 			return start;
 		}
-		
+
 		return -1;
 	}
-	
+
 }
--- eclipse-workspace/Search/src/com/leetcode/search/Solution.java
+++ eclipse-workspace/Search/src/com/leetcode/search/Solution.java
@@ -6,39 +6,85 @@ public class Solution {
 		// TODO Auto-generated method stub
 
 	}
-	
-	// 35. Search Insert Position 
-	// Description: Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order. 
+
+	// 35. Search Insert Position
+	// Description: Given a sorted array and a target value, return the index if the
+	// target is found. If not, return the index where it would be if it were
+	// inserted in order.
 	// Company: Google # Amazon
-	// Solution: Binary Search find the middle element, check which range it belongs to.
+	// Solution: Binary Search find the middle element, check which range it belongs
+	// to.
 	public int searchInsert(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        
-        while (left + 1 < right) {
-        	int mid = left + (right - left) / 2;
-        	if (nums[mid] < target) {
-        		left = mid;
-        	} else {
-        		right = mid;
-        	}
-        }
-        
-        if (nums[left] == target) {
-            return left;
-        }
-        
-        if (nums[right] == target) {
-            return right;
-        }
-        
-        if (target > nums[right]) {
-            return right + 1;
-        } else if (nums[left] > target) {
-            return left;
-        } else {  // in the middle
-            return left + 1;
-        }
-    }
+		int left = 0;
+		int right = nums.length - 1;
+
+		while (left + 1 < right) {
+			int mid = left + (right - left) / 2;
+			if (nums[mid] < target) {
+				left = mid;
+			} else {
+				right = mid;
+			}
+		}
+
+		if (nums[left] == target) {
+			return left;
+		}
+
+		if (nums[right] == target) {
+			return right;
+		}
+
+		if (target > nums[right]) {
+			return right + 1;
+		} else if (nums[left] > target) {
+			return left;
+		} else { // in the middle
+			return left + 1;
+		}
+	}
+
+	// 33. Search in Rotated Sorted Array
+	// Description:
+	// Company:
+	// Solution:
+	public int search(int[] nums, int target) {
+		if (nums == null || nums.length == 0) {
+			return -1;
+		}
+
+		int left = 0;
+		int right = nums.length - 1;
+
+		while (left + 1 < right) {
+			int mid = left + (right - left) / 2;
+			// 1. equal. 2. mid in left 3. mid in right
+			if (target == nums[mid]) {
+				return mid;
+			} else if (nums[mid] >= nums[left]) { // 2.
+				if (nums[left] <= target && nums[mid] > target) {
+					right = mid;
+				} else {
+					left = mid;
+				}
+			} else if (nums[mid] <= nums[right]) { // 3.
+				if (nums[mid] < target && nums[right] >= target) {
+					left = mid;
+				} else {
+					right = mid;
+				}
+			}
+		}
+
+		if (nums[left] == target) {
+			return left;
+		}
+
+		if (nums[right] == target) {
+			return right;
+		}
+
+		return -1;
+	}
 
 }
