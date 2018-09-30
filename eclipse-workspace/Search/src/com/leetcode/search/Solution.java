b3a7ecdb54ffc581e094e89851800e40df4976de
osx <osx@osxs-MacBook-Pro.local>
2018-09-30 11:01:03 -0700

    154. Find Minimum in Rotated Sorted Array II

--- eclipse-workspace/Search/src/com/leetcode/search/Solution.java
+++ eclipse-workspace/Search/src/com/leetcode/search/Solution.java
@@ -343,7 +343,9 @@ public class Solution {
 			
 			if (nums[mid] < nums[right]) {
 				right = mid;
-			} else if (nums[mid] == nums[right]){
+			} else if (nums[mid] > nums[right]){
+				left = ++mid;
+			} else {
 				right--;
 			}
 		}
