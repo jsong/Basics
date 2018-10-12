6ef96d0a7bc289513cab93c28da14798b2cb561a
Jian Song <simjian@gmail.com>
2018-10-12 15:01:30 -0700

    231. Power of Two

--- eclipse-workspace/Bit/src/com/leetcode/bit/Solution.java
+++ eclipse-workspace/Bit/src/com/leetcode/bit/Solution.java
@@ -7,4 +7,23 @@ public class Solution {
 
 	}
 
-}
+	// 231. Power of Two
+	// Company: Amazon
+	// Description: Given an integer, write a function to determine if it is a power of two.
+	// Solution: 1. % 2 until 0 or 1; 2. Highest bit should be 1, minus 1 other bits will be 1, bit& them to check whether it become 0.
+	public boolean isPowerOfTwo(int n) {
+		if (n == 2 || n == 1) {
+				 return true;
+		 }
+
+		if (n % 2 == 0) {
+		 	n = n / 2;
+		 	return isPowerOfTwo(n);
+	 	} else {
+		 return false;
+	 	}
+	}
+
+	public boolean isPowerOfTwo2(int n) {
+		return n > 0 && ((n&(n - 1)) == 0);
+	}
