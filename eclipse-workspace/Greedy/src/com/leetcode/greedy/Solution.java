8db4ff5e0a9633becca09bd9524d68b0ac0b703d
osx <osx@osxs-Mac-Pro.local>
2018-11-15 09:32:44 -0800

    Init draft for 121. Best Time to Buy and Sell Stock

--- eclipse-workspace/Greedy/src/com/leetcode/greedy/Solution.java
+++ eclipse-workspace/Greedy/src/com/leetcode/greedy/Solution.java
@@ -46,4 +46,24 @@ public class Solution {
 
 		return steps;
 	}
+
+	// 121. Best Time to Buy and Sell Stock
+	// Description:
+	// Company:
+	// Solution:
+	public int maxProfit(int[] prices) {
+		// int buy = 1;
+		int max_profit = 0;
+		for (int i = 0, int j= i + 1; i < prices.length, j < prices.length;) {
+			if (prices[i] > prices[j]) {
+				i++;
+				j++;
+			} else {
+				max_profit = Math.max(max_profit, prices[j] - prices[i]);
+				j++;
+			}
+		}
+
+		return max_profit;
+	}
 }
