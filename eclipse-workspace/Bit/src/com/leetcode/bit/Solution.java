cc9bb636980add725d71c1d5f3f3415a3b4dadee
Jian <jian@Jians-MacBook-Pro.local>
2019-02-28 23:21:07 -0800

    191. Number of 1 Bits 89. Gray Code 136. Single Number

--- eclipse-workspace/Bit/src/com/leetcode/bit/Solution.java
+++ eclipse-workspace/Bit/src/com/leetcode/bit/Solution.java
@@ -1,10 +1,16 @@
 package com.leetcode.bit;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Solution {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Solution sl = new Solution();
+		List<String> res = sl.findRepeatedDnaSequences("AAAAAAAAAAAAA");
+		System.out.println("Repeat: " + res);
 	}
 
 	// 231. Power of Two
@@ -50,17 +56,21 @@ public class Solution {
 	// 187. Repeated DNA Sequences
 	// Company: LinkedIn
 	// Description:
-	// Solution: TODO Memory profiling. 
+	// Solution: TODO Memory profiling.
 	public List<String> findRepeatedDnaSequences(String s) {
 		HashMap<String, Integer> map = new HashMap<>();
-		HashSet<String> set = new HashSet<>();
+		List<String> arr = new ArrayList<String>();
+		// HashSet<String> set = new HashSet<>();
 		for (int i = 0; i < s.length() - 9; i++)
 		{
 			String key = s.substring(i, i + 10); // 10 bits in total.
 			if (map.containsKey(key))
 			{
+				if (map.get(key) == 1)
+				{
+					arr.add(key);
+				}
 				map.put(key, map.get(key) + 1);
-				set.add(key);
 			}
 			else
 			{
@@ -68,9 +78,63 @@ public class Solution {
 			}
 		}
 
-		List<String> arr = new ArrayList<String>(set);
-
 		return arr;
+  }
+
+	// 191. Number of 1 Bits
+	// Company:
+	// Description:
+	// Solution:
+	public int hammingWeight(int n) {
+      int count = 0;
+			// while (n != 0)
+			for (int i = 0; i < 32; i++)
+			{
+				if ((n & 1) == 1)
+				{
+					count++;
+				}
+				n = n >>> 1;
+			}
+
+			return count;
+  }
+
+	// 89. Gray Code
+	// Company: Amazon
+	// Description:
+	// Solution:
+	public List<Integer> grayCode(int n) {
+			int loop = (int)Math.pow(2, n);
+			List<Integer> res  = new ArrayList<Integer>();
+			for (int i = 0; i < loop; i++)
+			{
+				res.add(i ^ (i >> 1));
+			}
+
+			return res;
+  }
+
+	// 136. Single Number
+	// Company: Airbnb Palantir
+	// Description:
+	// Solution: 0 XOR b equals to b, a XOR a = 0.
+	public int singleNumber(int[] nums) {
+		int res = 0;
+		for (int i = 0; i < nums.length; i++)
+		{
+			res ^= nums[i];
+		}
+
+		return res;
+	}
+
+	//137. Single Number II
+	// Company:
+	// Description:
+	// Solution:
+	public int singleNumber(int[] nums) {
+
   }
 
 }
