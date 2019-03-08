217d39eb812958b56d3edbcfd07bcedd58513b9e
Jian <jian@Jians-MacBook-Pro.local>
2019-03-07 23:10:48 -0800

    9. Palindrome Number

--- eclipse-workspace/ArthmaticProblem/src/com/leetcode/Solution.java
+++ eclipse-workspace/ArthmaticProblem/src/com/leetcode/Solution.java
@@ -1,3 +1,4 @@
+package com.leetcode;
 
 public class Solution {
 
@@ -5,7 +6,7 @@ public class Solution {
 		int result = 5/2;
 		System.out.println("result:"+result);
 	}
-	
+
 	public boolean canPlaceFlowers(int[] flowerbed, int n) {
 		int result = 0;
 		int counter = 1;
@@ -18,6 +19,43 @@ public class Solution {
 			}
 		}
 		if(counter > 0) result += counter/2;
-		return result > n;		
+		return result > n;
 	}
+
+	// 7. Reverse Integer
+	// Company: Apple NetEase Bloomberg
+	// Description:
+	// Solution:
+	public int reverse(int x) {
+		int res = 0;
+		while (x != 0)
+		{
+			if (Math.abs(res) > Integer.MAX_VALUE / 10) return 0; // overflow
+			res = res * 10 + x % 10;
+			x /= 10;
+		}
+
+		return res;
+  }
+
+	// 9. Palindrome Number
+	// Company:
+	// Description:
+	// Solution:
+	public boolean isPalindrome(int x) {
+		if (x < 0) return false;
+		String s = Integer.toString(x);
+		int i = 0;
+		int j = s.length() - 1;
+		while (i < j)
+		{
+						char h = s.charAt(i++);
+						char t = s.charAt(j--);
+			if (h != t)
+			{
+				return false;
+			}
+		}
+		return true;
+  }
 }
